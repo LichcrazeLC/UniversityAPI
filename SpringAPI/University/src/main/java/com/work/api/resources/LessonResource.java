75d97dcede59ea2bbbc8344ef7bcb2766b312350
LichcrazeLC <ciocanlaurentiu98@gmail.com>
2019-03-25 11:10:10 +0200

    (PRACT-2) finished Students resource controller with the appropriate endpoints and responses, added lesson resource controller with endpoints and responses.

---
+++ SpringAPI/University/src/main/java/com/work/api/resources/LessonResource.java
@@ -0,0 +1,62 @@
+package com.work.api.resources;
+
+import com.work.api.models.Lesson;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LessonResource {
+    private static Map< Integer, Lesson> lessonRepo = new HashMap<>();
+
+    static{
+        Lesson dataStructures = new Lesson(1,"Data Structures and Algorithms",
+                "Computers, Informatics and Microelectronics", "Software Engineering", 1);
+        Lesson internetOfThings = new Lesson(2,"Internet of Things",
+                "Computers, Informatics and Microelectronics", "Software Engineering",3);
+        lessonRepo.put(dataStructures.getLessonId(), dataStructures);
+        lessonRepo.put(internetOfThings.getLessonId(), internetOfThings);
+    }
+
+    @RequestMapping("/lessons")
+    public ResponseEntity<Object> getStudents() {
+        return new ResponseEntity<>(lessonRepo.values(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/lessons", method = RequestMethod.POST)
+    public ResponseEntity<Object> postLesson(@RequestBody Lesson lesson) {
+        lessonRepo.put(lesson.getLessonId(),lesson);
+        return new ResponseEntity<>("Lesson created successfully!", HttpStatus.CREATED);
+    }
+
+    @RequestMapping("/lessons/{lessonId}")
+    public ResponseEntity<Object> getLessonById(@PathVariable int lessonId) {
+        return new ResponseEntity<>(lessonRepo.get(lessonId), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/lessons/{lessonId}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> putLessonById(@PathVariable int lessonId, @RequestBody Lesson lesson) {
+        if (lessonRepo.containsKey(lessonId)) {
+            lessonRepo.put(lessonId, lesson);
+            return new ResponseEntity<>(lessonRepo.get(lessonId), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Lesson not found!", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/lessons/{lessonId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteLessonById(@PathVariable int lessonId) {
+        if (lessonRepo.containsKey(lessonId)) {
+            lessonRepo.remove(lessonId);
+            return new ResponseEntity<>("Lesson successfully deleted!", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Lesson not found!", HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
--- SpringAPI/University/src/main/java/com/work/api/resources/StudentsResource.java
+++ SpringAPI/University/src/main/java/com/work/api/resources/StudentsResource.java
@@ -3,10 +3,7 @@ package com.work.api.resources;
 import com.work.api.models.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,6 +29,31 @@ public class StudentsResource {
     public ResponseEntity<Object> postStudent(@RequestBody Student student) {
         studentRepo.put(student.getStudentId(), student);
         return new ResponseEntity<>("Student created successfully!", HttpStatus.CREATED);
+    }
+
+    @RequestMapping("/students/{studentId}")
+    public ResponseEntity<Object> getStudentById(@PathVariable int studentId) {
+        return new ResponseEntity<>(studentRepo.get(studentId), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/students/{studentId}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> putStudentById(@PathVariable int studentId, @RequestBody Student student) {
+        if (studentRepo.containsKey(studentId)) {
+            studentRepo.put(studentId, student);
+            return new ResponseEntity<>(studentRepo.get(studentId), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Student not found!", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/students/{studentId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteStudentById(@PathVariable int studentId) {
+        if (studentRepo.containsKey(studentId)) {
+            studentRepo.remove(studentId);
+            return new ResponseEntity<>("Student successfully deleted!", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Student not found!", HttpStatus.NOT_FOUND);
+        }
     }
 
 }
