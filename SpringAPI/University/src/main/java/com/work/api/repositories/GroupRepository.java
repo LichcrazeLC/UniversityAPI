c85c580e1d91c36bf97bd1dd4f2955ffc3f4a70c
LichcrazeLC <ciocanlaurentiu98@gmail.com>
2019-04-16 15:55:37 +0300

    added docker and docker-compose file, updated pom.xml and app properties to work with docker-compose

--- SpringAPI/University/src/main/java/com/work/api/repositories/GroupRepository.java
+++ SpringAPI/University/src/main/java/com/work/api/repositories/GroupRepository.java
@@ -8,7 +8,9 @@ import java.util.Optional;
 
 @Repository
 public interface GroupRepository extends JpaRepository <Group, Long> {
+
     Optional<Group> findByName(String name);
     Group getByName(String name);
     boolean existsByName(String name);
+
 }
--- SpringAPI/University/src/main/java/com/work/api/repositories/LessonRepository.java
+++ SpringAPI/University/src/main/java/com/work/api/repositories/LessonRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface LessonRepository extends JpaRepository <Lesson, Long> {
+
     Optional<Lesson> findByGroupNameAndId(String groupName, Long Id);
     List<Lesson> findAllByGroupName(String groupName);
 
